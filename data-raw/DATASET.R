7b7940431571751511224901d54ad2cf1bdde746
Fonti Kar <f.kar@unsw.edu.au>
2024-03-22 12:32:27 +1100

    Updated code to generate testing data

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -29,7 +29,7 @@ suppressWarnings( #Suppresses stan warnings
     rmot_assign_data(n_obs = 7, #integer
                      y_obs = seq(from=1, by=2.5, length.out=7) + rnorm(n=7, 0, 0.2),
                      obs_index = 1:7, #vector length N_obs
-                     time = rep(5, times=length(y_single)), #Vector length N_obs
+                     time = seq(from=0, to=30, by=5), #Vector length N_obs
                      y_0_obs = y_single[1] #vector length N_ind
     ) |>
     rmot_run(chains = 2, iter = 300, verbose = FALSE, show_messages = FALSE)
@@ -47,7 +47,7 @@ suppressWarnings( #Suppresses stan warnings
                      n_ind = 2, #integer
                      y_obs = y_multi, #vector length N_obs
                      obs_index = rep(seq(from=1, to=7, by=1), times=2), #vector length N_obs
-                     time = rep(5, times=length(y_multi)), #Vector length N_obs
+                     time = rep(seq(from=0, to=30, by=5), times=2), #Vector length N_obs
                      ind_id = c(rep(1, times=7), rep(2, times=7)), #Vector length N_obs
                      y_0_obs = y_multi[c(1, 8)] #vector length N_ind
     ) |>
