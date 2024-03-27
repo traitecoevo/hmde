d0f608e665ab27ce236a90f46b43e92f6a03101c
Fonti Kar <f.kar@unsw.edu.au>
2024-03-28 10:04:32 +1100

    Init comit for constant branch

--- tests/testthat/fixtures/constant/make_constant.R
+++ tests/testthat/fixtures/constant/make_constant.R
@@ -1,26 +1,3 @@
-#Generates data for constant growth model testing
-#Differential equation governing dynamics.
-DE <- function(y, pars){
-  return(pars[1])
-}
-
-#Normally distributed error
-add_error_process <- function(y, sigma_e=0.001){
-  return(y + rnorm(length(y), mean=0, sd=sigma_e))
-}
-
-#Function to generate distribution of DE parameters
-DE_par_generation <- function(n_ind, pars=list(mean=0, sd=1)){
-  par_sample <- data.frame(beta=exp(rnorm(n_ind, mean = pars[[1]], sd=pars[[2]])))
-  return(par_sample)
-}
-
-#Generate initial conditions
-initial_condition_generation <- function(n_ind, pars=list(mean=2, sd=1)){
-  y_0_sample <- data.frame(y_0=exp(rnorm(n_ind, mean = pars[[1]], sd=pars[[2]])))
-  return(y_0_sample)
-}
-
 #Set required values
 set.seed(2024) #Guarantees same data each time.
 model_name <- "constant"
@@ -44,6 +21,7 @@ y_obs <- add_error_process(true_data$y_true)
 rmot_export_test_data(n_obs_per_ind,
                       n_ind,
                       y_obs,
+                      time,
                       DE_pars,
                       initial_conditions,
                       true_data,
@@ -51,7 +29,7 @@ rmot_export_test_data(n_obs_per_ind,
 
 #Build model fit single ind
 set.seed(2024)
-const_data <- readRDS("tests/testthat/fixtures/constant/constant_data_single_ind.rds")
+const_data <- readRDS(test_path("fixtures/constant/constant_data_single_ind.rds"))
 suppressWarnings( #Suppresses stan warnings
   constant_single_ind_test <- rmot_model("constant_single_ind") |>
     rmot_assign_data(n_obs = const_data$n_obs, #integer
@@ -63,11 +41,12 @@ suppressWarnings( #Suppresses stan warnings
     rmot_run(chains = 1, iter = 300, verbose = FALSE, show_messages = FALSE)
 )
 saveRDS(rstan::summary(constant_single_ind_test)$summary,
-        file = "tests/testthat/fixtures/constant/constant_baseline_output_single_ind.rds")
+        file = test_path("fixtures/constant/constant_baseline_output_single_ind.rds")
+)
 
 #Build model fit multiple inds
 set.seed(2024)
-const_data <- readRDS("tests/testthat/fixtures/constant/constant_data_multi_ind.rds")
+const_data <- readRDS(test_path("fixtures/constant/constant_data_multi_ind.rds"))
 suppressWarnings( #Suppresses stan warnings
   constant_multi_ind_test <- rmot_model("constant_multi_ind") |>
     rmot_assign_data(n_obs = const_data$n_obs, #integer
@@ -81,6 +60,7 @@ suppressWarnings( #Suppresses stan warnings
     rmot_run(chains = 1, iter = 300, verbose = FALSE, show_messages = FALSE)
 )
 saveRDS(rstan::summary(constant_multi_ind_test)$summary,
-        file = "tests/testthat/fixtures/constant/constant_baseline_output_multi_ind.rds")
+        file = test_path("fixtures/constant/constant_baseline_output_multi_ind.rds")
+)
 
-set.seed(Sys.time())
+set.seed(NULL)
