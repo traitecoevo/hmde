0cff8208f4c2ac2ca5fbf8c3753b92835db2ba35
Theresa O'Brien <tesslacoil@protonmail.com>
2024-04-23 11:12:44 +1000

    Revert "Vb model add (#18)"
    This reverts commit 37693f7c5f65a2b116c771befca111bfbc9eea9e.

--- R/rmot_run.R
+++ R/rmot_run.R
@@ -22,9 +22,7 @@ rmot_run <- function(model_template, ...) {
          canham_single_ind = rstan::sampling(stanmodels$canham_single_ind, data = model_template, ...),
          canham_multi_ind = rstan::sampling(stanmodels$canham_multi_ind, data = model_template, ...),
          power_single_ind = rstan::sampling(stanmodels$power_single_ind, data = model_template, ...),
-         power_multi_ind = rstan::sampling(stanmodels$power_multi_ind, data = model_template, ...),
-         vb_single_ind = rstan::sampling(stanmodels$vb_single_ind, data = model_template, ...),
-         vb_multi_ind = rstan::sampling(stanmodels$vb_multi_ind, data = model_template, ...))
+         power_multi_ind = rstan::sampling(stanmodels$power_multi_ind, data = model_template, ...))
 
   return(out)
 }
