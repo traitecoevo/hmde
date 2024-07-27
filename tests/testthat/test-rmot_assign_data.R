e604b019e6c38f79ed530b35828f9550e44915a1
Tess-LaCoil <tesslacoil@protonmail.com>
2024-07-27 17:26:26 +1000

    Updated example data assignment to replace linear regression.

---
+++ tests/testthat/test-rmot_assign_data.R
@@ -0,0 +1,20 @@
+test_that("Execution and output", {
+  constant_single <- rmot_model("constant_single_ind") |>
+    rmot_assign_data(n_obs = 2,
+                     y_obs = c(1,2),
+                     obs_index = c(1,2),
+                     time = c(0,1),
+                     y_0_obs = 1)
+  
+  expect_named(constant_single)
+  
+  expect_visible(constant_single)
+  
+  expect_type(constant_single, "list")
+  
+  constant_empty <- rmot_model("constant_single_ind")
+  
+  expect_null(constant_empty$y_obs)
+  
+  expect_true(length(constant_single$y_obs) > 0)
+})
