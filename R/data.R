24c13e6404dfdc873f99745db58f7983c851cde3
Tess-LaCoil <tesslacoil@protonmail.com>
2024-11-13 11:05:08 +1100

    Updated lizard and tree data, the former to be length rather than mass, the latter to be G. recondita.

--- R/data.R
+++ R/data.R
@@ -2,6 +2,7 @@
 #'
 #' A subset of data from the SUSTAIN trout capture-recapture data set
 #' from Moe et al. (2020).
+#' Observations are of total body length in centimetres.
 #' Data prepared by taking a stratified sample of individual IDs based on the
 #' number of observations per individual: 25 individuals with 2 observations,
 #' 15 with 3, 10 with 4. Within the groups a simple random sample
@@ -24,21 +25,22 @@
 #'
 #' A subset of data from Kar, Nakagawa, and Noble (2024), used to model growth
 #' behaviour in a skink species.
+#' Observations are of the length from the tip of the nose to the start of the cloaca.
 #' Data was prepared by taking a simple random sample with replacement of 50
 #' individual IDs among individuals with at least 5 observations each. Data was
 #' then transformed to conform to the needs of a model data set in the package.
 #'
-#' @format ## `Lizard_Mass_Data`
+#' @format ## `Lizard_Size_Data`
 #' A data frame with 336 rows and 4 columns:
 #' \describe{
 #'   \item{ind_id}{ID number for individual}
 #'   \item{time}{Days since first observation.}
-#'   \item{y_obs}{Individual mass in kilograms.}
+#'   \item{y_obs}{Individual size in mm.}
 #'   \item{obs_index}{Index of observations for individual}
 #' }
 #'
 #' @source \url{https://osf.io/hjkxd/}
-"Lizard_Mass_Data"
+"Lizard_Size_Data"
 
 #' Protium tenufolium - Barro Colorado Island data
 #'
@@ -63,14 +65,14 @@
 #' @source \url{https://doi.org/10.15146/5xcp-0d46}
 "Small_Tree_Size_Data"
 
-#' Hirtella triandra - Barro Colorado Island data
+#' Garcinia recondita - Barro Colorado Island data
 #'
 #' A subset of data from the Barro Colorado Island long term forest plot
 #' managed by the Smithsonian Tropical Research Institute (Condit et al. 2019).
 #' Data was prepared by taking a simple random sample without replacement of
-#' 30 individual IDs from Hirtella triandra. The sampling frame was restricted
+#' 30 individual IDs from Garcinia recondita. The sampling frame was restricted
 #' to individuals with 6 observations since 1990, and a difference between
-#' observed max and min sizes of more than 7cm in order to avoid identifiability
+#' observed first and last sizes of more than 3cm in order to avoid identifiability
 #' issues. Data was then transformed and renamed to match the required structure
 #' to act as demonstration for the package.
 #'
