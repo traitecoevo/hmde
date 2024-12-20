e8d263afdf45ff49e5eff51836d5bbb0a9778b7e
Tess-LaCoil <tesslacoil@protonmail.com>
2024-12-20 17:21:37 +1100

    Updated data assignment and model names.

--- R/hmde_models.R
+++ R/hmde_models.R
@@ -119,6 +119,6 @@ hmde_linear_single_ind <- function(){
        y_bar = NULL,
        int_method = NULL,
        prior_means = c(1,1),
-       prior_sd = c(2,2,),
+       prior_sds = c(2,2),
        model = "linear_single_ind")
 }
