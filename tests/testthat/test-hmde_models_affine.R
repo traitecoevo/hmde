fe376e79c595e0572e1279591b3206f28e8a7e62
Tess-LaCoil <tesslacoil@protonmail.com>
2024-12-30 22:46:09 +1100

    Corrected testing for affine model to include the prior SDs.

--- tests/testthat/test-hmde_models_affine.R
+++ tests/testthat/test-hmde_models_affine.R
@@ -4,7 +4,7 @@ test_that("Model structures: affine", {
   single_model <- hmde_model("affine_single_ind")
   expect_named(single_model, c("step_size", "n_obs", "y_obs", "obs_index",
                                "time", "y_bar", "int_method", "prior_means",
-                               "model"))
+                               "prior_sds", "model"))
   expect_type(single_model, "list")
   expect_visible(single_model)
 })
