ae6c171bd0058fcb46132becf062f8e96e8bcc3e
Tess-LaCoil <tesslacoil@protonmail.com>
2025-07-25 16:48:41 +1000

    Initial structure of S3 methods for hmde_fit object.

---
+++ R/hmde_S3_methods.R
@@ -0,0 +1,43 @@
+#' Print function for hmdefit object
+#'
+#' @param fit output from hmde_run
+#'
+#' @examples
+#' # basic usage of hmde_assign_data
+#' hmde_model("constant_single_ind") |> hmde_assign_data(Trout_Size_Data)
+#'
+#' @export
+
+print.hmdefit <- function(fit = NULL){
+
+}
+
+
+#' Summary function for hmdefit object
+#'
+#' @param fit output from hmde_run
+#'
+#' @examples
+#' # basic usage of hmde_assign_data
+#' hmde_model("constant_single_ind") |> hmde_assign_data(Trout_Size_Data)
+#'
+#' @export
+
+summary.hmdefit <- function(fit = NULL){
+
+}
+
+
+#' Plot function for hmdefit object
+#'
+#' @param fit output from hmde_run
+#'
+#' @examples
+#' # basic usage of hmde_assign_data
+#' hmde_model("constant_single_ind") |> hmde_assign_data(Trout_Size_Data)
+#'
+#' @export
+
+plot.hmdefit <- function(fit = NULL){
+
+}
