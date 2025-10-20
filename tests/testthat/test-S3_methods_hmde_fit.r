252d96628c2344df6e7967fea3e045aa5e83ffba
Tess-LaCoil <tesslacoil@protonmail.com>
2025-10-20 19:49:35 +1100

    Completed testing for s3 plot functions.

--- tests/testthat/test-S3_methods_hmde_fit.r
+++ tests/testthat/test-S3_methods_hmde_fit.r
@@ -3,20 +3,28 @@ test_that("hmde_model_template: plot function for single ind models", {
   suppressWarnings(
     fit <- hmde_model("constant_single_ind") |>
       hmde_assign_data(Trout_Size_Data[1:5,])|>
-      hmde_run(chains = 1, iter = 1000,
+      hmde_run(chains = 1, iter = 100,
                verbose = FALSE, show_messages = FALSE)
   )
 
   output <- plot.hmde_fit(fit)
+
+  expect_visible(output)
+
+  expect_true(is_ggplot(output))
 })
 
 test_that("hmde_model_template: plot function for multiple-ind models", {
   suppressWarnings(
     fit <- hmde_model("constant_multi_ind") |>
       hmde_assign_data(Trout_Size_Data)|>
-      hmde_run(chains = 1, iter = 1000,
+      hmde_run(chains = 1, iter = 100,
                verbose = FALSE, show_messages = FALSE)
   )
 
   output <- plot.hmde_fit(fit)
+
+  expect_visible(output)
+
+  expect_true(is_ggplot(output))
 })
--- tests/testthat/test-S3_methods_hmde_model_template.r
+++ tests/testthat/test-S3_methods_hmde_model_template.r
@@ -2,4 +2,8 @@
 test_that("hmde_model_template: print function", {
   output <- hmde_model("constant_single_ind") |>
     plot.hmde_model_template()
+
+  expect_visible(output)
+
+  expect_true(is_ggplot(output))
 })
