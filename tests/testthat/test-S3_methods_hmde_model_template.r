fbf8f84cf29c9697ef93395d18277a9c25c101da
Tess-LaCoil <tesslacoil@protonmail.com>
2025-10-20 16:50:03 +1100

    Added initial testing framework.

--- tests/testthat/test-S3_methods_hmde_model_template.r
+++ tests/testthat/test-S3_methods_hmde_model_template.r
@@ -1,4 +1,18 @@
 #Testing for print function
-test_that("Model structures: affine", {
-  output <- hmde_model("constant_single_ind") |> print.hmde_model_template()
+test_that("hmde_model_template: print function", {
+  output <- hmde_model("constant_single_ind") |>
+    print.hmde_model_template()
+})
+
+#Testing for summary function
+test_that("hmde_model_template: print function", {
+  output <- hmde_model("constant_single_ind") |>
+    summary.hmde_model_template()
+})
+
+
+#Testing for plot function
+test_that("hmde_model_template: print function", {
+  output <- hmde_model("constant_single_ind") |>
+    plot.hmde_model_template()
 })
