b14fade2280848762cd87142db45244d572a3531
Tess-LaCoil <tesslacoil@protonmail.com>
2026-02-04 12:01:45 +1100

    Started building data template class.

--- R/hmde_data_template.R
+++ R/hmde_data_template.R
@@ -1,1 +1,20 @@
 #S4 methods for hmde_data_template
+
+# Class definition
+setClass(
+  Class = "hmde_data_template",
+  representation = representation(
+    model_name = "character", # Name of model DE
+    model_level = "character", # Single or multi-individual
+    obs_data = "list", # List of input data for model
+    prior_pars = "list" # List of prior parameters for model
+  ),
+  prototype = prototype(
+    model_name = NA_character_,
+    model_level = NA_character_,
+    obs_data = NULL,
+    prior_pars = NULL
+  )
+)
+
+# Class constructor
