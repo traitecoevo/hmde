7f48c3837fe7ca1c91ddcbecc6a9955b7225dd62
Tess-LaCoil <tesslacoil@protonmail.com>
2026-03-01 14:07:47 +1100

    Fixed hmde_data_template methods and built unit tests.

--- R/hmde_data_template.R
+++ R/hmde_data_template.R
@@ -56,10 +56,6 @@ setGeneric("obs_data", function(x) standardGeneric("obs_data"))
 setGeneric("obs_data<-", function(x, value) standardGeneric("obs_data<-"))
 setMethod("obs_data", "hmde_data_template", function(x) x@obs_data)
 setMethod("obs_data<-", "hmde_data_template", function(x, value) {
-  if(names(value) != names(x@obs_data)){
-    stop("List names do not match obs_data template.")
-  }
-
   x@obs_data <- value
   x
 })
@@ -69,10 +65,6 @@ setGeneric("prior_pars", function(x) standardGeneric("prior_pars"))
 setGeneric("prior_pars<-", function(x, value) standardGeneric("prior_pars<-"))
 setMethod("prior_pars", "hmde_data_template", function(x) x@prior_pars)
 setMethod("prior_pars<-", "hmde_data_template", function(x, value) {
-  if(names(value) != names(x@prior_pars)){
-    stop("Prior names do not match prior_pars template.")
-  }
-
   x@prior_pars <- value
   x
 })
@@ -84,17 +76,17 @@ setMethod("prior_pars<-", "hmde_data_template", function(x, value) {
 # Constructor
 hmde_data_template <- function(model_name, #Mandatory
                                model_level = NA_character_, #Optional
-                               obs_data = NA, #Optional
-                               prior_pars = NA, #Optional
+                               obs_data = NULL, #Optional
+                               prior_pars = NULL, #Optional
                                ...){  #Optional additional user input
   #Validation
-  if(!model %in% hmde_model_names()){
+  if(!model_name %in% hmde_model_names()){
     stop("Model name not recognised. Run hmde_model_names() to see available models.")
   }
 
   template <- hmde_model(model_name) #Construct new object with defaults for model
 
-  if(model_level(template) != model_level){
+  if(!is.na(model_level) && (model_level(template) != model_level)){
     stop("Provided model level does not agree with model name.")
   }
 
@@ -112,80 +104,104 @@ hmde_data_template <- function(model_name, #Mandatory
   data_fields_priors <- names(prior_pars(template))
 
   #If obs_data is provided, check it has required names
-  if(!is.na(obs_data)){
+  if(!is.null(obs_data)){
     #Check for name structure
-    if(names(obs_data) != data_fields_obs){
+    if(!identical(names(obs_data), data_fields_obs)){
       stop("Input observation data do not have correct names.")
     }
 
     obs_data(template) <- obs_data
-  } else { #Check if additional user fields are provided
-    if(model_level(template) == "multi_ind"){ # Multi-individual with ind_id vec
-      for(i in c("ind_id", "time", "y_obs", "obs_index")){
-        if(!i %in% user_fields){
-          stop(paste0("Improper data structure: ", i, " missing"))
-        }
-      }
-
-    } else { # Single individual models
-      for(i in c("time", "y_obs", "obs_index")){
-        if(!i %in% user_fields){
-          stop(paste0("Improper data structure: ", i, " missing"))
-        }
-      }
-    }
-
+  } else if(length(user_code) != 0
+            && length(intersect(data_fields_obs, user_fields)) > 0
+            ){ #Check if relevant user fields are provided
     #Construct list formation
     obs_data_temp_list <- obs_data(template)
     for(i in data_fields_obs){ # Iterate through required fields and fill them
       if(i %in% user_fields){ #Check if the user has supplied it in a tibble
         obs_data_temp_list <- purrr::list_modify(obs_data_temp_list, !!!additional_data[i])
-
       }
+    }
 
-      if(is.na(obs_data_temp_list[[i]])){ #Catches default tibble transformations
-        obs_data_temp_list[[i]] <- switch(
-          i,
-          n_obs = length(obs_data_temp_list$y_obs),
-          n_ind = length(unique(obs_data_temp_list$ind_id)),
-          y_bar = mean(obs_data_temp_list$y_obs)
-        )
+    for(i in intersect(data_fields_obs, c("n_obs", "n_ind", "y_bar"))){ # Check that fields are valid and filled.
+      if(length(obs_data_temp_list[[i]]) == 1){
+        if(is.na(obs_data_temp_list[[i]])){ #Catches default tibble transformations
+
+          obs_data_temp_list[[i]] <- switch(
+            i,
+            n_obs = length(obs_data_temp_list$y_obs),
+            n_ind = length(unique(obs_data_temp_list$ind_id)),
+            y_bar = mean(obs_data_temp_list$y_obs)
+          )
+        }
+
+        if(is.na(obs_data_temp_list[[i]])){ #Report missing data
+          stop(paste("Improper data structure: Data missing:", i))
+        }
       }
+    }
 
-      if(is.null(obs_data_temp_list[[i]])){ #Report missing data
-        stop(paste("Improper data structure: Data missing:", i))
+    #Validate that vectors have content
+    for(i in names(obs_data_temp_list)){
+      if((i != "n_obs") && length(obs_data_temp_list[[i]]) == 1){
+        if(is.na(obs_data_temp_list[[i]])){
+          stop(paste("Data missing:", i))
+        }
+      }
+    }
+
+    #Validate lengths of vectors
+    check_length <- obs_data_temp_list[["n_obs"]]
+    for(i in names(obs_data_temp_list)){
+      if(i != "n_obs"){
+        if(length(obs_data_temp_list[[i]]) != check_length){
+          stop(paste("Mismatch in length between n_obs = ",
+                     check_length,  "and", i,
+                     "with length",
+                     length(obs_data_temp_list[[i]])))
+        }
       }
     }
 
     obs_data(template) <- obs_data_temp_list
   }
 
-  if(!is.na(prior_pars)){
+  if(!is.null(prior_pars)){
     #Check for name structure
-    if(names(prior_pars) != data_fields_priors){
+    if(!identical(names(prior_pars), data_fields_priors)){
       stop("Input prior parameters do not have correct names.")
+    }
+
+    #Check for vector lengths
+    for(i in data_fields_priors){
+      if(length(prior_pars[[i]]) != length(prior_pars(template)[[i]])){
+        stop(paste("Input priors do not have correct lengths:", i))
+      }
     }
 
     prior_pars(template) <- prior_pars
 
   #Check if non-default user fields are provided
-  } else if(length(intersect(user_fields, data_fields_priors)) > 0){
-    #Construct list formation
-    prior_par_temp_list <- prior_pars(template) #Take default priors as base structure
-    for(i in data_fields_priors){ # Iterate through required fields and update them
-      if(i %in% user_fields){ #Check if the user has supplied it in a tibble
-        #Validate prior parameter length
-        if(length(!!!additional_data[i]) != length(data_fields_priors[[i]])){
-          stop(paste0("Length of parameters for ", i,
-                      " incorrect. Should be ", length(data_fields_priors[[i]]),
-                      "."))
+  } else if(length(user_code) != 0
+            && length(intersect(data_fields_priors, user_fields)) > 0){
+    if(length(intersect(user_fields, data_fields_priors)) > 0){
+      #Construct list formation
+      prior_par_temp_list <- prior_pars(template) #Take default priors as base structure
+
+      for(i in data_fields_priors){ # Iterate through required fields and update them
+        if(i %in% user_fields){ #Check if the user has supplied values
+          prior_par_temp_list <- purrr::list_modify(prior_par_temp_list, !!!additional_data[i])
+
+          #Validate prior parameter length
+          if(length(prior_par_temp_list[[i]]) != length(template@prior_pars[[i]])){
+            stop(paste0("Length of parameters for ", i,
+                        " incorrect. Should be ", length(template@prior_pars[[i]]),
+                        "."))
+          }
         }
-
-        prior_par_temp_list <- purrr::list_modify(prior_par_temp_list, !!!additional_data[i])
       }
-    }
 
-    prior_pars(template) <- prior_par_temp_list
+      prior_pars(template) <- prior_par_temp_list
+    }
   }
 
   #Check validity
---
+++ tests/testthat/test-hmde_data_template.R
@@ -0,0 +1,147 @@
+test_that("hmde_data_template: initialisaiton and validation", {
+  # Default initialisation should not error
+  test_object <- new("hmde_data_template")
+  expect_true(is(test_object, "hmde_data_template"))
+
+  # Attempt to assign to non-existent field
+  expect_error(new("hmde_data_template", invalid_field = 1))
+
+  # Test internal type validation
+  expect_error(new("hmde_data_template", model_name = 1))
+  expect_error(new("hmde_data_template", model_level = 1))
+  expect_error(new("hmde_data_template", obs_data = 1))
+  expect_error(new("hmde_data_template", prior_pars = 1))
+
+  # Test length validation for model_name and model_type
+  expect_error(new("hmde_data_template", model_name = c("one", "two")))
+  expect_error(new("hmde_data_template", model_type = c("one", "two")))
+})
+
+test_that("hmde_data_template: setters and getters", {
+  test_object <- new("hmde_data_template")
+
+  # model_name
+  model_name(test_object) <- "test_value"
+  expect_equal(model_name(test_object), "test_value")
+  expect_error(model_name(test_object) <- 1) # Bad type
+
+  # model_level
+  model_level(test_object) <- "test_value"
+  expect_equal(model_level(test_object), "test_value")
+  expect_error(model_level(test_object) <- 1) # Bad type
+
+  # obs_data
+  obs_data(test_object) <- list(test_name = "test_value")
+  expect_equal(obs_data(test_object), list(test_name = "test_value"))
+  expect_error(obs_data(test_object) <- 1) # Bad type
+
+  # prior_pars
+  prior_pars(test_object) <- list(test_name = "test_value")
+  expect_equal(prior_pars(test_object), list(test_name = "test_value"))
+  expect_error(obs_data(test_object) <- 1) # Bad type
+})
+
+test_that("hmde_data_template: constructor", {
+  # Just model name provided
+  test_object <- hmde_data_template(model_name = "constant_single_ind")
+  # Check default values
+  expect_equal(model_name(test_object), "constant_single_ind")
+  expect_equal(model_level(test_object), "single_ind")
+  expect_equal(obs_data(test_object), list(n_obs = NA_integer_,
+                                           y_obs = NA_real_,
+                                           obs_index = NA_integer_,
+                                           time = NA_real_))
+  expect_equal(prior_pars(test_object), list(
+    prior_pars_ind_beta = c(0, 2),
+    prior_pars_global_error_sigma = c(0, 2)
+  ))
+
+  # Invalid model name
+  expect_error(hmde_data_template(model_name = "wrong_name"))
+
+  # Testing different methods of data assignment
+  # Filling obs_data
+  obs_data <-list(
+    n_obs = 3,
+    y_obs = c(1,1,1),
+    obs_index = 1:3,
+    time = 0:2
+  )
+  test_object <- hmde_data_template(model_name = "constant_single_ind",
+                                    obs_data = obs_data)
+  expect_equal(obs_data(test_object), obs_data)
+
+  test_object <- hmde_data_template(model_name = "constant_single_ind",
+                                    n_obs = 3,
+                                    y_obs = c(1,1,1),
+                                    obs_index = 1:3,
+                                    time = 0:2)
+  expect_equal(obs_data(test_object), obs_data)
+
+  test_object <- hmde_data_template(model_name = "constant_single_ind",
+                                    y_obs = c(1,1,1),
+                                    obs_index = 1:3,
+                                    time = 0:2)
+  expect_equal(obs_data(test_object), obs_data)
+
+  #Mismatched vector lengths
+  expect_error(hmde_data_template(model_name = "constant_single_ind",
+                                  y_obs = c(1),
+                                  obs_index = 1:3,
+                                  time = 0:2))
+  expect_error(hmde_data_template(model_name = "constant_single_ind",
+                                  y_obs = c(1,1,1),
+                                  obs_index = 1,
+                                  time = 0:2))
+  expect_error(hmde_data_template(model_name = "constant_single_ind",
+                                  y_obs = c(1,1,1),
+                                  obs_index = 1:3,
+                                  time = 0))
+
+  #Missing data
+  expect_error(hmde_data_template(model_name = "constant_single_ind",
+                                  obs_index = 1:3,
+                                  time = 0:2))
+  expect_error(hmde_data_template(model_name = "constant_single_ind",
+                                  y_obs = c(1,1,1),
+                                  time = 0:2))
+  expect_error(hmde_data_template(model_name = "constant_single_ind",
+                                  y_obs = c(1,1,1),
+                                  obs_index = 1:3))
+
+  #Filling prior_pars
+  prior_pars <- list(
+    prior_pars_ind_beta = c(1,2),
+    prior_pars_global_error_sigma = c(1,3)
+  )
+  test_object <- hmde_data_template(model_name = "constant_single_ind",
+                                    prior_pars = prior_pars)
+  expect_equal(prior_pars(test_object), prior_pars)
+
+  test_object <- hmde_data_template(model_name = "constant_single_ind",
+                                    prior_pars_ind_beta = c(1,2),
+                                    prior_pars_global_error_sigma = c(1,3))
+  expect_equal(prior_pars(test_object), prior_pars)
+
+  #Modify only one
+  test_object <- hmde_data_template(model_name = "constant_single_ind",
+                                    prior_pars_ind_beta = c(1,2))
+  expect_equal(prior_pars(test_object), list(
+    prior_pars_ind_beta = c(1,2),
+    prior_pars_global_error_sigma = c(0,2)
+  ))
+
+  #Bad prior inputs
+  expect_error(hmde_data_template(model_name = "constant_single_ind",
+                                  prior_pars = list(bad_name = 1)))
+  expect_error(hmde_data_template(model_name = "constant_single_ind",
+                                  prior_pars =
+                                    list(prior_pars_ind_beta = c(1,2),
+                                         prior_pars_global_error_sigma = c(1,3),
+                                         extra_entry = 1)))
+  expect_error(hmde_data_template(model_name = "constant_single_ind",
+                                  prior_pars =
+                                    list(prior_pars_ind_beta = c(1,2),
+                                         prior_pars_global_error_sigma = 1)))
+
+})
