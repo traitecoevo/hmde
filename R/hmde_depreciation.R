03b6582d240bf166ca91958bb3ee97d1a58bd550
Tess-LaCoil <tesslacoil@protonmail.com>
2026-04-06 13:10:07 +1000

    Added depreciation messages for hmde_assign_data and hmde_extract_estimates.

---
+++ R/hmde_depreciation.R
@@ -0,0 +1,16 @@
+#Depreciation messages for prior use functions
+
+#' Depreciated function: hmde_assign_data
+#' @noRd
+hmde_assign_data <- function(...){
+  stop("hmde_assign_data() is depreciated.
+       Please use hmde_data_template() instead.")
+}
+
+
+#' Depreciated function: hmde_extract_estimates
+#' @noRd
+hmde_extract_estimates <- function(...){
+  stop("hmde_extract_estimates() is depreciated.
+       Please use hmde_estimates() instead.")
+}
